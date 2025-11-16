6192551a2fc8c08bc6ecefaa71a057b8a34ca54e
Daik <fruit@kd6.so-net.ne.jp>
2025-11-16 21:41:21 +0900

    Refactor the code

--- 05_analyze-array/analyze-array.js
+++ 05_analyze-array/analyze-array.js
@@ -9,18 +9,17 @@ const analyzeArray = (array) => {
     }
   }
 
-  const result = {};
   const average = +(array.reduce((acc, cur) => acc + cur, 0) / array.length).toFixed(2);
   const min = array.sort((a, b) => a - b)[0];
   const max = array.sort((a, b) => b - a)[0];
   const length = array.length;
 
-  result.average = average;
-  result.min = min;
-  result.max = max;
-  result.length = length;
-
-  return result;
+  return { 
+    average, 
+    min, 
+    max, 
+    length 
+  };
 }
 
 module.exports = analyzeArray;
